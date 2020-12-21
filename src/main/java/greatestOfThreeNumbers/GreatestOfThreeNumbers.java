eac15017fa533d6ea1a58d8a1e2965c8ff743126
Saismita <saismitamohanty100@gmail.com>
2020-12-21 10:42:36 -0800

    Activity

---
+++ src/main/java/greatestOfThreeNumbers/GreatestOfThreeNumbers.java
@@ -0,0 +1,26 @@
+package greatestOfThreeNumbers;
+
+import java.util.Scanner;
+public class GreatestOfThreeNumbers
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the first number : ");
+        int first = sc.nextInt();
+        System.out.print("Enter the second number : ");
+        int second = sc.nextInt();
+        System.out.print("Enter the third number : ");
+        int third = sc.nextInt();
+        System.out.println();
+        if(first > second && first > third)
+            System.out.println(first+" is the greatest number.");
+        else if(second > first && second > third)
+            System.out.println(second+" is the greatest number.");
+        else if(third > first && third > second)
+            System.out.println(third+" is the greatest number.");
+        else
+            System.out.println("All three numbers are same");
+        sc.close();
+    }
+}
